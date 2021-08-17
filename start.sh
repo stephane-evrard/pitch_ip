498c49bc226843967706bc7022fd541e426db8bc
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-17 19:54:26 +0300

    postgres url

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- $env: postgres://dtoqaoutneurti:543d86a61d67f1dd7acd3de98d8460df91dc608fd3aa62563f29a3c49c458a01@ec2-52-3-130-181.compute-1.amazonaws.com:5432/d2vi8najlsnu2
+ $env: postgres://arfpkkfhaqnhvz:e2e6150483c3be642c412477dc9096dd79827f3de7a102593d11b2c1b36581ae@ec2-52-3-130-181.compute-1.amazonaws.com:5432/d3amqe9c2tfg1 
 
  $env:SECRET_KEY='powerfulsecretkey'
 
