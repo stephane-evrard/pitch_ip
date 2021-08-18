3440a70c9b5818739949cd65c8a28f318eab5a15
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-18 10:04:28 +0300

    secret key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- $env: postgres://arfpkkfhaqnhvz:e2e6150483c3be642c412477dc9096dd79827f3de7a102593d11b2c1b36581ae@ec2-52-3-130-181.compute-1.amazonaws.com:5432/d3amqe9c2tfg1 
+ $env: postgres://qwlijmtfovyadb:c8387d615588b75dc61b9ca394405bbfd7f30722b31a5a8748ddcd6c40184a3d@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d7lgb6jnk89607
 
  $env:SECRET_KEY='powerfulsecretkey'
 
