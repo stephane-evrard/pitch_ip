bad085fc2bc695533af09d980377095f91593108
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-19 11:30:27 +0300

    development

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- $env: postgres://qwlijmtfovyadb:c8387d615588b75dc61b9ca394405bbfd7f30722b31a5a8748ddcd6c40184a3d@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d7lgb6jnk89607
+#  $env: postgres://qwlijmtfovyadb:c8387d615588b75dc61b9ca394405bbfd7f30722b31a5a8748ddcd6c40184a3d@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d7lgb6jnk89607
 
  $env:SECRET_KEY='powerfulsecretkey'
 
